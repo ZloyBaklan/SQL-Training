35929fe950c4522aa51e7d01cf6ca2fb9bf353d0
Sergey <75798404+ZloyBaklan@users.noreply.github.com>
2021-10-05 18:07:03 +0300

    Update and rename answer to answer.sql

---
+++ task-2/answer.sql
@@ -0,0 +1,8 @@
+SELECT name, membercost FROM cd.facilities
+
+'''
+For this question, we need to specify the columns that we want. 
+We can do that with a simple comma-delimited list of column names specified to the select statement.
+All the database does is look at the columns available in the FROM clause, 
+and return the ones we asked for, as illustrated below
+'''
