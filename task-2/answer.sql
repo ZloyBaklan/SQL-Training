97de40912561959c78a5b370166915aba920e9d7
Sergey <75798404+ZloyBaklan@users.noreply.github.com>
2021-10-05 18:08:49 +0300

    Update answer.sql

--- task-2/answer.sql
+++ task-2/answer.sql
@@ -6,7 +6,7 @@ We can do that with a simple comma-delimited list of column names specified to t
 All the database does is look at the columns available in the FROM clause, 
 and return the ones we asked for.
 
-Generally speaking, for non-throwaway queries it's considered desirable to specify 
+Generally speaking, for non-throwaway queries its considered desirable to specify 
 the names of the columns you want in your queries rather than using *. 
 This is because your application might not be able to cope if more columns get added into the table.
 '''
