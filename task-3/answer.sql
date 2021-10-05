b05298602a517c9a5b61772547420108f56dfa40
Sergey <75798404+ZloyBaklan@users.noreply.github.com>
2021-10-05 18:09:17 +0300

    Update answer.sql

--- task-3/answer.sql
+++ task-3/answer.sql
@@ -5,9 +5,9 @@ The FROM clause is used to build up a set of candidate rows to read results from
 this set of rows has simply been the contents of a table. In future we will explore joining, 
 which allows us to create much more interesting candidates.
 
-Once we've built up our set of candidate rows, 
-the WHERE clause allows us to filter for the rows we're interested in - in this case, 
+Once weve built up our set of candidate rows, 
+the WHERE clause allows us to filter for the rows were interested in - in this case, 
 those with a membercost of more than zero. As you will see in later exercises,
-WHERE clauses can have multiple components combined with boolean logic - it's possible to, 
+WHERE clauses can have multiple components combined with boolean logic - its possible to, 
 for instance, search for facilities with a cost greater than 0 and less than 10.
 '''
